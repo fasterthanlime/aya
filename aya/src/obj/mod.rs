1ff42eb24159cfef86e555c814b5a6aa13f21e11
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-02-10 11:27:22 +0900

    Fix unit test

--- aya/src/obj/mod.rs
+++ aya/src/obj/mod.rs
@@ -1256,7 +1256,7 @@ mod tests {
         assert_matches!(
             obj.parse_section(fake_section(
                 BpfSectionKind::Program,
-                "socket_filter/foo",
+                "socket/foo",
                 bytes_of(&fake_ins())
             )),
             Ok(())
